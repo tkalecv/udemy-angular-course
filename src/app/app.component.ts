69cf68b0ef606f7bf7f31550388a0636fd6d84e1
Valentin Tkalec <valentintkalec@hotmail.com>
2022-06-14 20:57:18 +0200

    Added navigation with event binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayRecipes: boolean = true;
 
+  onRecipeShowedHidden(value: boolean){
+    this.displayRecipes = value;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +8,11 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   collapsed = true;
+  @Output() showRecipe = new EventEmitter<boolean>();
+
+  onShowHideRecipe(value: boolean) {
+    this.showRecipe.emit(value);
+  }
 
   constructor() { }
 
