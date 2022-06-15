b4b72f7baab0604b06c371a678fd07b4a9094570
Valentin Tkalec <valentintkalec@hotmail.com>
2022-06-15 17:40:40 +0200

    Added show details when recipe is clicked

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,14 +7,23 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() showRecipeDetails = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
-    new Recipe("Test Recipe",
+    new Recipe("Test Recipeeeeeee",
      "This is a test recipe",
      "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/image_data/file/108320/s960_pizza-slice.jpg"),
      new Recipe("Test Recipe",
      "This is a test recipe",
+     "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/image_data/file/108320/s960_pizza-slice.jpg"),
+     new Recipe("Recipe Test",
+     "This is a test recipe",
      "https://assets.publishing.service.gov.uk/government/uploads/system/uploads/image_data/file/108320/s960_pizza-slice.jpg")
   ];
+
+  onRecipeSelected(selectedRecipe: Recipe){
+    this.showRecipeDetails.emit(selectedRecipe)
+  }
 
   constructor() { }
 
